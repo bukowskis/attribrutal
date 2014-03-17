fa9a80eb8676699e5121a93d9c1978e20b4951ca
Peter Stiernström <peter.stiernstrom@bukowskis.com>
2014-03-17 16:03:50 +0100

    Nitpicking.

--- spec/attributable_spec.rb
+++ spec/attributable_spec.rb
@@ -4,15 +4,15 @@ describe "Attributable#attributes" do
 
   let (:bar) { Bar.new foo: nil, bar: 10, baz: { alpha: 10, beta: "20" } }
 
-  it "should record defined attributes" do
+  it "records defined attributes" do
     Bar.attributes.keys.should == [:foo, :bar, :baz]
   end
 
-  it "should record defined attribute types" do
+  it "records defined attribute types" do
     Bar.attributes.values.should == [Coercer::Boolean, Coercer::String, Coercer::Baz]
   end
 
-  it "should support mass assignment" do
+  it "supports mass assignment" do
     bar.raw_attributes.values.should == [nil, 10, { alpha: 10, beta: "20" }]
     bar.attributes[:foo].should == false
     bar.attributes[:bar].should == "10"
@@ -20,13 +20,13 @@ describe "Attributable#attributes" do
     bar.attributes[:baz].beta.should == 20
   end
 
-  it "should perform coercion" do
+  it "performs coercion" do
     attributes = bar.attributes
     attributes[:foo].should == false
     attributes[:bar].should == "10"
   end
 
-  it "should support recursive coercion" do
+  it "supports recursive coercion" do
     attributes = bar.attributes
     attributes[:baz].alpha.should == 10
     attributes[:baz].beta.should == 20
